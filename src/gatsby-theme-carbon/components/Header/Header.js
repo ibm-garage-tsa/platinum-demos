28e89079e91189af4c7aee93f73afd3eee15e84f
Dave Whiteley <wdave@ca.ibm.com>
2021-08-18 16:34:30 -0700

    Header updates

--- src/gatsby-theme-carbon/components/Header/Header.js
+++ src/gatsby-theme-carbon/components/Header/Header.js
@@ -2,19 +2,17 @@ import React, {  } from 'react';
 import {
   Header as ShellHeader,
   SkipToContent,
-  HeaderGlobalBar,
 } from 'carbon-components-react';
 import cx from 'classnames';
 
 import HeaderNav from 'gatsby-theme-carbon/src/components/HeaderNav/HeaderNav';
-import GlobalSearch from 'gatsby-theme-carbon/src/components/GlobalSearch';
 import useMetadata from 'gatsby-theme-carbon/src/util/hooks/useMetadata';
 
 import styles from 'gatsby-theme-carbon/src/components/Header/Header.module.scss';
 
 const Header = ({ children }) => {
 
-  const { isSearchEnabled, navigationStyle } = useMetadata();
+  const { navigationStyle } = useMetadata();
 
   return (
     <ShellHeader
@@ -26,10 +24,7 @@ const Header = ({ children }) => {
       <SkipToContent href="#main-content" className={styles.skipToContent} />
 
       {navigationStyle && <HeaderNav />}
-      <HeaderGlobalBar>
-        {isSearchEnabled && <GlobalSearch />}
 
-      </HeaderGlobalBar>
     </ShellHeader>
   );
 };
