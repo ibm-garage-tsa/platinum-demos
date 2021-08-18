2160fd6d1b5265d2af54963a05cc42cea8c1b33f
Dave Whiteley <wdave@ca.ibm.com>
2021-08-18 08:02:28 -0700

    300-BA Aug 18 update

--- src/gatsby-theme-carbon/components/header/index.js
+++ src/gatsby-theme-carbon/components/header/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = (props) => (
-  
+  <Header {...props}>
+    <span>Gatsby theme</span>&nbsp;Carbon
+  </Header>
 );
 
 export default CustomHeader;
