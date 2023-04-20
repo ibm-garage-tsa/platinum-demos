d35a8663cf4b15768623fde622ea926b81d24f54
Dave Whiteley <wdave@ca.ibm.com>
2023-04-20 13:03:25 -0700

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,16 +11,31 @@ exports.onCreateWebpackConfig = ({ actions }) => {
     const { createRedirect } = actions;
   
     createRedirect({
-      fromPath: '/300-business-automation',
-      toPath: '/300-business-automation-straight-through-processing',
+      fromPath: '/300-business-automation/demo-preparation',
+      toPath: '/300-business-automation-straight-through-processing/demo-preparation',
+      isPermanent: true,
+      redirectInBrowser: true
+    });
+
+    createRedirect({
+      fromPath: '/300-business-automation/demo-script',
+      toPath: '/300-business-automation-straight-through-processing/demo-script',
       isPermanent: true,
       redirectInBrowser: true
     });
   
     createRedirect({
-        fromPath: '/300-integration',
-        toPath: '/300-integration/demo-script',
-        isPermanent: true,
-        redirectInBrowser: true
-      });
-    };
+      fromPath: '/300-integration/demo-preparation',
+      toPath: '/300-integration-api-enabled-application-integration//demo-preparation',
+      isPermanent: true,
+      redirectInBrowser: true
+    });
+
+    createRedirect({
+      fromPath: '/300-integration/demo-script',
+      toPath: '/300-integration-api-enabled-application-integration/demo-script',
+      isPermanent: true,
+      redirectInBrowser: true
+    });
+  };
+  
